a5e02be3daea34e4d68e807d49c6ab4faf5b6c9b
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2025-09-17 21:02:20 -0300

    adicionando algumas besteiras no codigo experimental

--- front-end/JavaScript/aula01.js
+++ front-end/JavaScript/aula01.js
@@ -1,5 +1,24 @@
 // Exibe uma mensagem ao carregar a página
-alert("Olá, mundooo wooow!");
+alert("Olá, mundo! 🎉");
 
-console.log("Olá, mundo!");
 // Mensagem no console para desenvolvedores
+console.log("Olá, mundo!");
+
+// Saudação personalizada
+const saudacao = "Bem-vindo ao curso de Front-End!";
+console.log(saudacao);
+
+// Declaração de variáveis numéricas
+const numero1 = 10;
+const numero2 = 20;
+
+// Cálculo da soma
+const resultadoSoma = numero1 + numero2;
+
+// Exibe o resultado da soma no console
+console.log(`A soma de ${numero1} + ${numero2} é: ${resultadoSoma}`);
+
+// Verifica o tipo da variável resultadoSoma
+console.log(`O tipo da variável 'resultadoSoma' é: ${typeof resultadoSoma}`);
+// Verifica o tipo da variável saudacao
+console.log(`O tipo da variável 'saudacao' é: ${typeof saudacao}`);
